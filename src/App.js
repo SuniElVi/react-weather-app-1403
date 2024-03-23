d9b5b043684dc5c862c155502f3e01297d871531
SunitaMousavi <107063207+SuniElVi@users.noreply.github.com>
2024-03-23 15:24:44 +0330

    Added The Link To GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-
+      <div>
+        This project was coded by Sunita Mousavi and is{" "}
+        <a
+          href="https://github.com/SuniElVi/react-weather-app-1403"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://warm-zuccutto-1fbcab.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </div>
     </div>
   );
 }
